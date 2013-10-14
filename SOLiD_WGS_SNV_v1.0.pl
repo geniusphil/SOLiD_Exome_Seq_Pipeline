42aa5542e34509cf8529694053a446cb7b9cf8ec
Philippe <genius.philip@gmail.com>
2013-10-15 03:42:39 +0800

    Update SOLiD_WGS_SNV_v1.0.pl
    Change parameter and command line.

--- SOLiD_WGS_SNV_v1.0.pl
+++ SOLiD_WGS_SNV_v1.0.pl
@@ -4,13 +4,15 @@
 ## SOLiD Whole Genome Sequencing SNV calling Pipeline
 ## 
 ## Software update:
-##                  GATK 2.3-9
-##                  Picard 1.85
-##                  samtools 0.1.18
+##                  GATK 2.7-2
+##                  Picard 1.98
+##                  samtools 0.1.98
 ##
-## reference: /mnt/mibNGS_abi_bio_ExonCaptures/Analysis/Reference/humanDna.fasta
-## known site: /mnt/mibNGS_abi_bio_ExonCaptures/Analysis/Reference/hg19.dbsnp.vcf
+## Reference: /mnt/mibNGSapps/Reference/humanDna.fasta
+## Known site: /mnt/mibNGSapps/Reference/hg19.dbsnp.vcf
+## 
 ## server: Cluster server
+## 
 ##
 
 
@@ -21,8 +23,8 @@ sub Usage{
     print STDERR <<EOF;
     SOLiD Exome Seq. SNP calling Pipeline script
     
-    -r reference
-    -k known site
+    -r reference (path: /mnt/mibNGSapps/Reference/humanDna.fasta)
+    -k known site (path: /mnt/mibNGSapps/Reference/hg19.dbsnp.vcf)
     -b bam file prefix
     -h Help
     
@@ -36,27 +38,42 @@ my $knownsite = $opt{k} or &Usage();
 my $prefix = $opt{b} or &Usage();
 $opt{h} and &Usage();
 
-## GATK RealignerTargetCreator
-`java -jar /opt/GenomeAnalysisTK-2.3-9/GenomeAnalysisTK.jar -T RealignerTargetCreator -nt 4 -R $ref -I $prefix.ma.bam -known $knownsite -o $prefix.ma.bam.intervals`;
-## GATK IndelRealigner
-`java -jar /opt/GenomeAnalysisTK-2.3-9/GenomeAnalysisTK.jar -T IndelRealigner -R $ref -I $prefix.ma.bam -targetIntervals $prefix.ma.bam.intervals -known $knownsite -o $prefix.realigned.bam`;
+##############################
+print "Check SOLiD BAM file index";
+`/mnt/mibNGSapps/opt/samtools-0.1.19/samtools index "$prefix"_Merge.bam`;
 
-## Picard Make duplicates
-`java -jar /opt/picard-tools-1.85/picard-tools-1.85/MarkDuplicates.jar I=$prefix.realigned.bam O=$prefix.realigned.rmdup.bam METRICS_FILE=$prefix.duplicate_report.txt VALIDATION_STRINGENCY=LENIENT REMOVE_DUPLICATES=true ASSUME_SORTED=true`;
+## GATK RealignerTargetCreator
+## Emits intervals for the Local Indel Realigner to target for realignment.
+`java -Xmx2g -jar /mnt/mibNGSapps/opt/GenomeAnalysisTK-2.7-2/GenomeAnalysisTK.jar -T RealignerTargetCreator -log $prefix.RealignerTargetCreator.log -nt 4 -R $ref -I "$prefix"_Merge.bam -known $knownsite -o "$prefix"_Merge.bam.intervals`;
+
+## GATK IndelRealigner
+## Performs local reaglignment of reads to correct misalignments due to the presence of indels.
+`java -Xmx2g -jar /mnt/mibNGSapps/opt/GenomeAnalysisTK-2.7-2/GenomeAnalysisTK.jar -T IndelRealigner -log $prefix.IndelRealigner.log -R $ref -I "$prefix"_Merge.bam -targetIntervals "$prefix"_Merge.bam.intervals -known $knownsite -o $prefix.realigned.bam`;
+
+## Picard Mark Duplicates
+`java -Xmx2g -jar /mnt/mibNGSapps/opt/picard-tools-1.98/MarkDuplicates.jar I=$prefix.realigned.bam O=$prefix.realigned.rmdup.bam METRICS_FILE=$prefix.duplicate_report.txt VALIDATION_STRINGENCY=LENIENT REMOVE_DUPLICATES=false ASSUME_SORTED=true`;
 
 ## SAMtools BAM file index
-`samtools index $prefix.realigned.rmdup.bam`;
+`/mnt/mibNGSapps/opt/samtools-0.1.19/samtools index $prefix.realigned.rmdup.bam`;
+
 ## SAMtools BAM file stat
-`samtools flagstat $prefix.realigned.rmdup.bam > $prefix.realigned.rmdup.bam.stat`;
+`/mnt/mibNGSapps/opt/samtools-0.1.19/samtools flagstat $prefix.realigned.rmdup.bam > $prefix.realigned.rmdup.bam.stat`;
 
 ## GATK BaseRecalibrator
-`java -jar /opt/GenomeAnalysisTK-2.3-9/GenomeAnalysisTK.jar -T BaseRecalibrator -nt 4 -R $ref -I $prefix.realigned.rmdup.bam -knownSites $knownsite -o $prefix.recal_data.grp --covariate QualityScoreCovariate --covariate ReadGroupCovariate --covariate ContextCovariate --covariate CycleCovariate --solid_nocall_strategy PURGE_READ --solid_recal_mode SET_Q_ZERO_BASE_N`;
-## GATK PrintReads
-`java -jar /opt/GenomeAnalysisTK-2.3-9/GenomeAnalysisTK.jar -T PrintReads -R $ref -I $prefix.realigned.rmdup.bam -BQSR $prefix.recal_data.grp -o $prefix.realigned.rmdup.recali.bam`;
-## GATK UinfiedGenotyper
-`java -jar /opt/GenomeAnalysisTK-2.3-9/GenomeAnalysisTK.jar -T UnifiedGenotyper -nt 4 -R $ref -I $prefix.realigned.rmdup.recali.bam -o "$prefix"_gatk.vcf -stand_call_conf 30.0 -stand_emit_conf 10.0 -glm both -D $knownsite`;
+## First pass of the base quailty score recalibration -- Generates recalibration table based on various user-specified covariates.
+## (such as read group, reported quality score, machine cycle, and nucleotide context.)
+`java -Xmx2g -jar /mnt/mibNGSapps/opt/GenomeAnalysisTK-2.7-2/GenomeAnalysisTK.jar -T BaseRecalibrator -log $prefix.BaseRecalibrator.log -nt 4 -R $ref -I $prefix.realigned.rmdup.bam -knownSites $knownsite -o $prefix.recal_data.table --covariate QualityScoreCovariate --covariate ReadGroupCovariate --covariate ContextCovariate --covariate CycleCovariate --solid_nocall_strategy PURGE_READ --solid_recal_mode SET_Q_ZERO_BASE_N`;
 
+## GATK PrintReads
+## Renders, in SAM/BAM format, all reads from the input data set in the order in which they appear in the input file.
+`java -Xmx2g -jar /mnt/mibNGSapps/opt/GenomeAnalysisTK-2.7-2/GenomeAnalysisTK.jar -T PrintReads -log $prefix.PrintReads.log -R $ref -I $prefix.realigned.rmdup.bam -BQSR $prefix.recal_data.table -o $prefix.realigned.rmdup.recali.bam`;
+
+## GATK UinfiedGenotyper
+## A variant caller which unifies the approaches of several disparate callers -- Works for single-sample and multi-sample data.
+`java -Xmx2g -jar /mnt/mibNGSapps/opt/GenomeAnalysisTK-2.7-2/GenomeAnalysisTK.jar -T UnifiedGenotyper -log $prefix.UnifiedGenotyper.log -nt 4 -R $ref -I $prefix.realigned.rmdup.recali.bam -o "$prefix"_gatk.vcf -stand_call_conf 30.0 -stand_emit_conf 10.0 -glm both -D $knownsite`;
+
+##############################
 ## SAMtools mpileup
-`samtools mpileup -ugf $ref $prefix.realigned.rmdup.recali.bam | bcftools view -bcvg - > $prefix.samtools.var.raw.bcf`;
-`bcftools view $prefix.samtools.var.raw.bcf | vcfutils.pl varFilter -D 500 > "$prefix"_samtools.vcf`;
+`/mnt/mibNGSapps/opt/samtools-0.1.19/samtools mpileup -ugf $ref $prefix.realigned.rmdup.recali.bam | /mnt/mibNGSapps/opt/samtools-0.1.19/bcftools/bcftools view -bcvg - > $prefix.samtools.var.raw.bcf`;
+`/mnt/mibNGSapps/opt/samtools-0.1.19/bcftools/bcftools view $prefix.samtools.var.raw.bcf | /mnt/mibNGSapps/opt/samtools-0.1.19/bcftools/vcfutils.pl varFilter -D 500 > "$prefix"_samtools.vcf`;
 
